ca654105eac02ec155a5ee5c12916e35e12fa129
Hyago <hyagojmaria@gmail.com>
2026-03-31 17:15:31 -0300

    Adicionando aprendizados sobre PEP8 e as bibliotecas Flake8 e Black

---
+++ PEP8 e Formatação com Flake8/script.py
@@ -0,0 +1,5 @@
+# PEP8 É uma convenção de como se deve escrever códigos em Python, por exemplo, ao criar uma função 'def somar(x, y):' por exemplo, nunca podemos colocar um espaço entre o nome da função e os parenteses onde ficam os parametros da função, isso pra todas as funções que você for criar, o uso do snake_case também é uma convenção que veio da PEP8. Também é uma convenção, de que uma linha nunca pode ter mais do que 79 caracteres, caso isso aconteça, precisamos achar uma forma de quebrar essa linha.
+
+# Flake8, é uma biblioteca do python que auxilia avisando caso o código esteja fora do (padrão) do PEP8, informando o desenvolvedor através do terminal
+
+# Black, é uma biblioteca assim como o Flake8, no entando, essa biblioteca formata o código ao invés de apenas avisar caso haja algum erro no código
